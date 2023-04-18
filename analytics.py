4d60b574ee911c991de61c2525e5d1bf97163b04
Leo Anthias <leo@datapane.com>
2023-04-18 11:19:45 +0100

    (task): convert to app, wip

--- analytics.py
+++ analytics.py
@@ -5,17 +5,13 @@ import warnings
 
 import altair as alt
 import folium
-import matplotlib
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 from dominate.tags import *
 from folium import plugins
 from mlxtend.frequent_patterns import apriori, association_rules
 
 warnings.filterwarnings("ignore")
 
-alt.data_transformers.enable("default", max_rows=None)
 locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
 
@@ -99,12 +95,24 @@ def plot_customer_locations(
 
 
 def plot_aov_histogram(df: pd.DataFrame) -> alt.Chart:
+    # Calculate the range of the data and create 10 equal-width bins
+    data_range = df["Total"].max() - df["Total"].min()
+    bin_width = data_range / 10
+    bins = [df["Total"].min() + i * bin_width for i in range(11)]
+
+    # Bin the data using pd.cut() with the calculated bins
+    binned_data = pd.cut(df["Total"], bins)
+
+    # Group by the bins and count the number of items in each bin
+    bin_counts = df.groupby(binned_data).size().reset_index(name='count')
+
+    # Update the Altair chart to use the new bin_counts DataFrame
     fig = (
-        alt.Chart(df)
-        .mark_bar(color="#00B3FE")
+        alt.Chart(bin_counts)
+        .mark_bar()
         .encode(
-            alt.X("Total:Q", bin=True, axis=alt.Axis(format="$f"), title=None),
-            alt.Y("count()", title=None),
+            alt.X("Total:O", title=None),
+            alt.Y("count:Q", title=None),
         )
         .properties(
             title=f"Average order value {locale.currency(df.Total.mean(), grouping=True)}"
@@ -112,16 +120,18 @@ def plot_aov_histogram(df: pd.DataFrame) -> alt.Chart:
     )
     return fig
 
+  
+
 
 def plot_value_counts(
-    series: pd.DataFrame, title: str, scale: str = "linear", bar_color: str = "#5A5BC1"
+    series: pd.DataFrame, title: str
 ) -> alt.Chart:
     fig = (
         alt.Chart(series)
-        .mark_bar(color=bar_color)
+        .mark_bar()
         .encode(
             x=alt.X("unique_values:N", title=None, sort=None),
-            y=alt.Y("counts:Q", scale=alt.Scale(type=scale), title=None),
+            y=alt.Y("counts:Q", title=None),
         )
         .properties(title=title)
     )
@@ -203,13 +213,45 @@ def frequent_product_combinations(df_items_window: pd.DataFrame) -> pd.DataFrame
     return frequent_combinations
 
 
+def orders_by_customer(df):
+    orders = (
+        (
+            df["Cust_ID"]
+            .value_counts()
+            .value_counts()
+            .rename_axis("unique_values")
+            .to_frame("counts")
+        )
+        .reset_index()
+        .rename(columns={0: "counts"})
+    )
+    return orders
+
+
+def orders_by_day(df):
+    orders = (
+        (
+            df["Created at"]
+            .dt.day_name()
+            .value_counts()
+            .rename_axis("unique_values")
+            .to_frame("counts")
+        )
+        .reindex(
+            ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        )
+        .reset_index()
+        .rename(columns={0: "counts"})
+    )
+    return orders
+
 def get_month(x: datetime.datetime) -> datetime.datetime:
     return datetime.datetime(x.year, x.month, 1)
 
-
 def cohort_analysis(
     df_orders_window: pd.DataFrame,
-) -> t.Tuple[matplotlib.figure.Figure, matplotlib.figure.Figure]:
+) -> t.Tuple[alt.Chart, alt.Chart]:
+
     df_orders_cohort = df_orders_window
 
     df_orders_cohort["order_month"] = df_orders_cohort["Created at"].apply(get_month)
@@ -224,62 +266,44 @@ def cohort_analysis(
         day = df[column].dt.day
         return year, month, day
 
-    # Getting the integers for date parts from the `InvoiceDay` column
     transcation_year, transaction_month, _ = get_date_int(
         df_orders_cohort, "order_month"
     )
 
-    # Getting the integers for date parts from the `CohortDay` column
     cohort_year, cohort_month, _ = get_date_int(df_orders_cohort, "cohort_month")
 
-    #  Get the  difference in years
     years_diff = transcation_year - cohort_year
 
-    # Calculate difference in months
     months_diff = transaction_month - cohort_month
 
     df_orders_cohort["cohort_index"] = years_diff * 12 + months_diff + 1
 
-    # Counting daily active user from each chort
     grouping = df_orders_cohort.groupby(["cohort_month", "cohort_index"])
 
-    # Counting number of unique customer Id's falling in each group of CohortMonth and CohortIndex
     cohort_data = grouping["Cust_ID"].apply(pd.Series.nunique)
     cohort_data = cohort_data.reset_index()
 
-    # Assigning column names to the dataframe created above
     cohort_counts = cohort_data.pivot(
         index="cohort_month", columns="cohort_index", values="Cust_ID"
     )
 
-    ### Retention rate
     cohort_sizes = cohort_counts.iloc[:, 0]
     retention = cohort_counts.divide(cohort_sizes, axis=0)
     retention.index = retention.index.strftime("%Y-%m")
 
-    retention_fig = plt.figure(figsize=(16, 10))
-    plt.rc("font", size=20)
-    plt.title(
-        "Retention Rate in percentage: Monthly Cohorts",
-    )
-    sns.heatmap(
-        retention,
-        annot=True,
-        fmt=".0%",
-        cmap="cividis_r",
-        vmin=0.0,
-        vmax=0.6,
-    )
-    plt.ylabel(
-        "Cohort Month",
-    )
-    plt.xlabel(
-        "Cohort Index",
-    )
-    plt.yticks(rotation=360)
-    plt.close()
+    retention = retention.reset_index().melt('cohort_month', var_name='cohort_index', value_name='retention_rate')
 
-    ### Average order total monthly cohort
+    retention_chart = alt.Chart(retention).mark_rect().encode(
+        x=alt.X('cohort_index:O', title="Cohort Index"),
+        y=alt.Y('cohort_month:O', title="Cohort Month"),
+        color=alt.Color('retention_rate:Q', legend=alt.Legend(format=".0%")),
+        tooltip=[alt.Tooltip('cohort_month:O', title='Cohort Month'), alt.Tooltip('cohort_index:O', title='Cohort Index'), alt.Tooltip('retention_rate:Q', format='.0%', title='Retention Rate')]
+    ).properties(
+        width=600,
+        height=400,
+        title="Retention Rate in percentage: Monthly Cohorts"
+    )
+
     grouping = df_orders_cohort.groupby(["cohort_month", "cohort_index"])
     cohort_data = grouping["Total"].mean()
     cohort_data = cohort_data.reset_index()
@@ -289,21 +313,19 @@ def cohort_analysis(
 
     average_standard_cost = average_order.round(1)
     average_standard_cost.index = average_standard_cost.index.strftime("%Y-%m")
+    
+    average_standard_cost = average_standard_cost.reset_index().melt('cohort_month', var_name='cohort_index', value_name='average_order_total')
 
-    avg_order_fig = plt.figure(figsize=(16, 10))
-    plt.title("Average Order Total: Monthly Cohorts")
-    sns.heatmap(
-        average_standard_cost,
-        annot=True,
-        vmin=0.0,
-        vmax=60,
-        cmap="cividis_r",
-        fmt="g",
+    avg_order_chart = alt.Chart(average_standard_cost).mark_rect().encode(
+        x=alt.X('cohort_index:O', title="Cohort Index"),
+        y=alt.Y('cohort_month:O', title="Cohort Month"),
+        color=alt.Color('average_order_total:Q', legend=alt.Legend(format="g")),
+        tooltip=[alt.Tooltip('cohort_month:O', title='Cohort Month'), alt.Tooltip('cohort_index:O', title='Cohort Index'), alt.Tooltip('average_order_total:Q', format='.1f', title='Average Order Total')]
+    ).properties(
+        width=600,
+        height=400,
+        title="Average Order Total: Monthly Cohorts"
     )
-    plt.ylabel("Cohort Month")
-    plt.xlabel("Cohort Index")
-    plt.yticks(rotation=360)
-    plt.xticks(fontsize=20)
-    plt.close()
 
-    return retention_fig, avg_order_fig
+    return retention_chart, cohort_data, avg_order_chart, average_standard_cost
+
